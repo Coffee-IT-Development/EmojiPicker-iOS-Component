4c7d47c6dbac4cb5efb0cddd8f9c43ee62ced509
Hugo de Groot <hugodegroot@coffeeit.nl>
2022-06-03 11:45:49 +0200

    Added hello world view

--- CITEmojiPicker/Sources/CITEmojiPicker/CITEmojiPicker.swift
+++ CITEmojiPicker/Sources/CITEmojiPicker/CITEmojiPicker.swift
@@ -1,6 +1,20 @@
-public struct CITEmojiPicker {
-    public private(set) var text = "Hello, World!"
+//
+//  CITEmojiPicker.swift
+//  CITEmojiPicker
+//
+//  Created by Hugo de Groot on 03/06/2022.
+//  Copyright © 2022 Coffee IT. All rights reserved.
+//
 
+import SwiftUI
+
+@available(iOS 13.0, *)
+public struct CITEmojiPicker: View {
+    public var body: some View {
+        Text("Hello World!")
+    }
+    
     public init() {
+        
     }
 }
