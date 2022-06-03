ef915eba4e3adaf379534a253e1b107dbaa54984
Hugo de Groot <hugodegroot@coffeeit.nl>
2022-06-03 13:25:18 +0200

    Added module to color extension

--- Sources/CITEmojiPicker/Essentials/Color+ColorStyles.swift
+++ Sources/CITEmojiPicker/Essentials/Color+ColorStyles.swift
@@ -9,9 +9,9 @@ import Foundation
 import SwiftUI
 
 public extension Color {
-    public static let emojiCategoriesBackground = Color("emoji_categories_background")
-    public static let searchBarBackground = Color("search_bar_background")
-    public static let selectedCategoryBackground = Color("selected_category_background")
-    public static let sheetBackground = Color("sheet_background")
-    public static let textColor = Color("text_color")
+    public static let emojiCategoriesBackground = Color("emoji_categories_background", bundle: .module)
+    public static let searchBarBackground = Color("search_bar_background", bundle: .module)
+    public static let selectedCategoryBackground = Color("selected_category_background", bundle: .module)
+    public static let sheetBackground = Color("sheet_background", bundle: .module)
+    public static let textColor = Color("text_color", bundle: .module)
 }
