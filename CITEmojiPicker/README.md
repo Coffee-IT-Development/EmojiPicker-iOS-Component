65a1dbd0105e1f88e7f61e2ad8670195e98c8adf
Hugo de Groot <hugodegroot@coffeeit.nl>
2022-06-03 11:44:00 +0200

    Initial commit

---
+++ CITEmojiPicker/README.md
@@ -0,0 +1,3 @@
+# CITEmojiPicker
+
+A description of this package.
---
+++ CITEmojiPicker/Sources/CITEmojiPicker/CITEmojiPicker.swift
@@ -0,0 +1,6 @@
+public struct CITEmojiPicker {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
