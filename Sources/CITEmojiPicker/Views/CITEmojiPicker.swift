3616cf5d74c8f8ff7ae8d6eaaf3d7f5c89031e5a
Hugo de Groot <hugodegroot@coffeeit.nl>
2022-06-16 11:44:49 +0200

    updated max height

--- Sources/CITEmojiPicker/Views/CITEmojiPicker.swift
+++ Sources/CITEmojiPicker/Views/CITEmojiPicker.swift
@@ -90,7 +90,7 @@ public struct CITEmojiPicker: View {
             }
         }
         .background(Color.sheetBackground)
-        .frame(maxWidth: .infinity, maxHeight: 390)
+        .frame(maxWidth: .infinity, maxHeight: 389)
     }
     
     public init(didAddEmoji: @escaping (String) -> Void) {
