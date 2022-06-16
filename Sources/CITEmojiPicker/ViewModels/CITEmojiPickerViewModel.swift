0ee604c8acf8630e5ed9394626328392ba88a02d
Hugo de Groot <hugodegroot@coffeeit.nl>
2022-06-16 10:40:37 +0200

    Added search emoji

--- Sources/CITEmojiPicker/ViewModels/CITEmojiPickerViewModel.swift
+++ Sources/CITEmojiPicker/ViewModels/CITEmojiPickerViewModel.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 public class CITEmojiPickerViewModel: ObservableObject {
-    public let emojiGroups: EmojiGroups
-    
     @Published var emojisByGroup: EmojiGroups = [:]
+    @Published var searchEmojis = [String]()
+    
+    public let emojiGroups: EmojiGroups
+    public var searchEmojiText = ""
+    private var searchEmojiArray = [EmojisByGroup]()
     
     public init() {
         emojiGroups = JSONFileDecoder.decodeEmojis()
         emojisByGroup = filterEmojis()
+        fillSearchEmojiList()
     }
     
     public func filterEmojis() -> EmojiGroups {
@@ -29,6 +33,26 @@ public class CITEmojiPickerViewModel: ObservableObject {
             emojiGroup = []
         }
         return emojisByGroup
+    }
+    
+    func fillSearchEmojiList() {
+        var emojiArray = [EmojisByGroup]()
+        for emojiType in EmojiTypes.allCases {
+            for emoji in emojisByGroup[emojiType.rawValue] ?? [] {
+                emojiArray.append(emoji)
+            }
+        }
+        searchEmojiArray = emojiArray
+    }
+    
+    func updateSearchEmojiList() {
+        var searchEmojisList = [String]()
+        for emoji in searchEmojiArray {
+            if emoji.name.contains(searchEmojiText.lowercased()) {
+                searchEmojisList.append(emoji.emoji)
+            }
+        }
+        searchEmojis = searchEmojisList
     }
 }
 
--- Sources/CITEmojiPicker/Views/CITEmojiPicker.swift
+++ Sources/CITEmojiPicker/Views/CITEmojiPicker.swift
@@ -12,69 +12,115 @@ public struct CITEmojiPicker: View {
     @ObservedObject public var viewModel = CITEmojiPickerViewModel()
     @State var selectedSection: EmojiTypes = .smileysAndEmotion
     @State var emojiPreferenceKeys: [EmojiPreferenceKey] = []
+    @State var isSearchingForEmoji: Bool = false
     private let didAddEmoji: (String) -> Void
     
     public var body: some View {
         VStack {
-            ScrollViewReader { reader in
+            HStack {
+                Image("ic_magnifying_glass", bundle: .module)
+                    .renderingMode(.template)
+                    .foregroundColor(Color.textColor)
+                    .padding(.leading)
+                
+                TextField("Search Emoji", text: $viewModel.searchEmojiText)
+                    .foregroundColor(Color.textColor)
+                    .keyboardType(.alphabet)
+                    .disableAutocorrection(true)
+//                    .placeholder(when: viewModel.searchEmojiText.isEmpty) {
+//                        Text("Search Emoji")
+//                            .foregroundColor(Color.textColor)
+//                    }
+            }
+            .frame(height: 36)
+            .background(Color.searchBarBackground)
+            .cornerRadius(10)
+            .padding(.horizontal, 16)
+            
+            VStack(spacing: 0) {
                 ScrollView(.horizontal) {
                     HStack {
-                        ForEach(EmojiTypes.allCases, id: \.rawValue) { emojiType in
-                            VStack(alignment: .leading, spacing: 0) {
-                                Text(emojiType.rawValue)
-                                    .foregroundColor(.textColor)
-                                    .padding([.bottom, .leading], 8)
-                                
-                                UIGrid(columns: 5, list: viewModel.emojisByGroup[emojiType.rawValue] ?? []) { emoji in
-                                    Text(emoji.emoji)
-                                        .font(.system(size: 28))
-                                        .padding(.horizontal, 7)
-                                        .padding(.vertical, 3)
-                                        .onTapGesture {
-                                            didAddEmoji(emoji.emoji)
-                                        }
+                        ForEach(viewModel.searchEmojis) { emoji in
+                            Text(emoji)
+                                .font(.system(size: 28))
+                                .onTapGesture {
+                                    didAddEmoji(emoji)
                                 }
-                            }
-                            .id(emojiType)
-                            .background(
-                                GeometryReader { proxy in
-                                    if emojiPreferenceKeys.count < EmojiTypes.allCases.count {
-                                        let yOffSet = proxy.frame(in: .named("emoji")).minX
-                                        let _ = DispatchQueue.main.async {
-                                            emojiPreferenceKeys.append(
-                                                EmojiPreferenceKey(
-                                                    emojiType: emojiType,
-                                                    yOffset: yOffSet
-                                                )
-                                            )
-                                        }
-                                    }
-                                    Color.clear
-                                }
-                            )
                         }
                     }
-                    .background(GeometryReader {
-                        Color.clear.preference(
-                            key: YOffsetScrollValuePreferenceKey.self,
-                            value: -$0.frame(in: .named("emoji")).origin.x
-                        )
-                    })
-                    .onPreferenceChange(YOffsetScrollValuePreferenceKey.self) { viewYOffsetKey in
-                        DispatchQueue.main.async {
-                            guard !emojiPreferenceKeys.isEmpty else { return }
-                            // This is added because the extra offset is needed for when you click on the EmojiPickerBottomNavigator
-                            let extraOffSet: CGFloat = 16
-                            for emojiPreferenceKey in emojiPreferenceKeys where emojiPreferenceKey.yOffset <= viewYOffsetKey + extraOffSet {
-                                selectedSection = emojiPreferenceKey.emojiType
-                            }
+                    .frame(alignment: .leading)
+                    .padding([.top, .horizontal], 16)
+                    .onChange(of: viewModel.searchEmojiText) { _ in
+                        viewModel.updateSearchEmojiList()
+                        if viewModel.searchEmojiText.isEmpty {
+                            isSearchingForEmoji = false
+                        } else {
+                            isSearchingForEmoji = true
                         }
                     }
                 }
-                .coordinateSpace(name: "emoji")
-            
-                EmojiPickerBottomNavigatorView(selectedSection: $selectedSection, reader: reader)
-                    .padding(.horizontal, 16)
+            }
+            if !isSearchingForEmoji {
+                ScrollViewReader { reader in
+                    ScrollView(.horizontal) {
+                        HStack {
+                            ForEach(EmojiTypes.allCases, id: \.rawValue) { emojiType in
+                                VStack(alignment: .leading, spacing: 0) {
+                                    Text(emojiType.rawValue)
+                                        .foregroundColor(.textColor)
+                                        .padding([.bottom, .leading], 8)
+                                    
+                                    UIGrid(columns: 5, list: viewModel.emojisByGroup[emojiType.rawValue] ?? []) { emoji in
+                                        Text(emoji.emoji)
+                                            .font(.system(size: 28))
+                                            .padding(.horizontal, 7)
+                                            .padding(.vertical, 3)
+                                            .onTapGesture {
+                                                didAddEmoji(emoji.emoji)
+                                            }
+                                    }
+                                }
+                                .id(emojiType)
+                                .background(
+                                    GeometryReader { proxy in
+                                        if emojiPreferenceKeys.count < EmojiTypes.allCases.count {
+                                            let yOffSet = proxy.frame(in: .named("emoji")).minX
+                                            let _ = DispatchQueue.main.async {
+                                                emojiPreferenceKeys.append(
+                                                    EmojiPreferenceKey(
+                                                        emojiType: emojiType,
+                                                        yOffset: yOffSet
+                                                    )
+                                                )
+                                            }
+                                        }
+                                        Color.clear
+                                    }
+                                )
+                            }
+                        }
+                        .background(GeometryReader {
+                            Color.clear.preference(
+                                key: YOffsetScrollValuePreferenceKey.self,
+                                value: -$0.frame(in: .named("emoji")).origin.x
+                            )
+                        })
+                        .onPreferenceChange(YOffsetScrollValuePreferenceKey.self) { viewYOffsetKey in
+                            DispatchQueue.main.async {
+                                guard !emojiPreferenceKeys.isEmpty else { return }
+                                // This is added because the extra offset is needed for when you click on the EmojiPickerBottomNavigator
+                                let extraOffSet: CGFloat = 16
+                                for emojiPreferenceKey in emojiPreferenceKeys where emojiPreferenceKey.yOffset <= viewYOffsetKey + extraOffSet {
+                                    selectedSection = emojiPreferenceKey.emojiType
+                                }
+                            }
+                        }
+                    }
+                    .coordinateSpace(name: "emoji")
+                
+                    EmojiPickerBottomNavigatorView(selectedSection: $selectedSection, reader: reader)
+                        .padding(.horizontal, 16)
+                }
             }
         }
         .frame(maxWidth: .infinity, maxHeight: 320)
