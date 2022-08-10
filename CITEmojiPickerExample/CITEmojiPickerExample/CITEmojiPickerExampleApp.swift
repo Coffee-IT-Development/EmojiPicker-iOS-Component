e0d0b30a39accd7c19e15ae587652ece4380a983
Hugo de Groot <hugodegroot@coffeeit.nl>
2022-08-10 14:33:45 +0200

    Implemented CITEmojiPickerExample app

---
+++ CITEmojiPickerExample/CITEmojiPickerExample/CITEmojiPickerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CITEmojiPickerExampleApp.swift
+//  CITEmojiPickerExample
+//
+//  Created by Hugo de Groot on 10/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CITEmojiPickerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
